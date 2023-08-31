e241004a04ae2225505e11abe633ad75c90d6c38
Sagar Thapa <Sagar.Thapa@pharmalex.com>
2023-08-31 12:26:46 +0100

    Add project files.

---
+++ azureDeploymentApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace azureDeploymentApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
